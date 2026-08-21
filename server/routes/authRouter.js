39da475f606ffe9f0b8726a12b74d0953d39346f
Sam Hadfield <sthtech@protonmail.com>
2026-08-21 11:16:56 -0500

    feat: import passport

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const passport = require("passport");
 const authController = require("../controllers/authController");
 
 router.post("/signup", authController.createNewUser);
 router.post("/login", (req, res) => {
-  // This is going to include Passport middleware
+  // This is going to include Pag
   res.json({
     message: "User is logged in",
   });
