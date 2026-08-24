5792f24d4753f5bd82ce2827799a630643c46d6a
Sam Hadfield <sthtech@protonmail.com>
2026-08-24 14:53:30 -0500

    feat: create and return JWT from login route
    Create and return a JWT from the /login route after performing user
    authentication with passport-local strategy. This returned a JWT to the
    client where it will be kept in localStorage and sent back to the server
    each time a request is made

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,13 +2,22 @@ const router = require("express").Router();
 const passport = require("passport");
 const authController = require("../controllers/authController");
 const validateUserSignup = require("../middleware/validateUserSignup");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", validateUserSignup, authController.createNewUser);
-router.post("/login", (req, res) => {
-  // This is going to include Pag
-  res.json({
-    message: "User is logged in",
-  });
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) return res.status(400).json({ message: "Login failed", info });
+
+    jwt.sign({ user }, process.env.SECRET_KEY, (err, token) => {
+      res.json({
+        token,
+      });
+    });
+  })(req, res, next);
 });
 
 module.exports = router;
