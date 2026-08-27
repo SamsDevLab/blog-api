c97835b4b1617b6f6d7a7490facca83363ba45f3
Sam Hadfield <sthtech@protonmail.com>
2026-08-27 13:59:49 -0500

    refactor: redo routing to reflect new components
    Redo routing and add children components. Ensure that Home is the index
    component

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,18 @@
 import App from "./App.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
+import SignUp from "./Components/SignUp/SignUp.jsx";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <SignUp /> },
+    ],
   },
-  { path: "login", element: <Login /> },
 ];
 
 export default routes;
