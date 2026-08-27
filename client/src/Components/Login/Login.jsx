0b6bafefa2b18b48f9d8f72a18341d3b5af99361
Sam Hadfield <sthtech@protonmail.com>
2026-08-27 13:55:17 -0500

    feat: add Components and export for routing

---
+++ client/src/Components/Login/Login.jsx
@@ -0,0 +1,7 @@
+import styles from "../Login/Login.module.css";
+
+const Login = () => {
+  return <h2>This is the login page!</h2>;
+};
+
+export default Login;
---
+++ client/src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import styles from "../SignUp/SignUp.module.css";
+
+const SignUp = () => {
+  return <h2>This is the sign up page!</h2>;
+};
+
+export default SignUp;
