ad08dcb7b8f1c044c35330b0f63d596f616274a1
Sam Hadfield <sthtech@protonmail.com>
2026-08-27 13:59:03 -0500

    feat: add Links to nav with wireframe styling

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import styles from "../Header/Header.module.css";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
     <header class={styles.header}>
       <h1>Sam's Dev Blog</h1>
+      <nav class={styles.navBar}>
+        <ul>
+          <li>
+            <Link to="login">Log In</Link>
+          </li>
+          <li>
+            <Link to="signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
