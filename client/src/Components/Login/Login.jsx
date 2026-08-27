348ba86af50de8cee7e4571bc4c65d3692f7e5ab
Sam Hadfield <sthtech@protonmail.com>
2026-08-27 15:38:11 -0500

    feat: create Login form - add wireframe styling

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,7 +1,30 @@
 import styles from "../Login/Login.module.css";
+import { Link } from "react-router";
 
 const Login = () => {
-  return <h2>This is the login page!</h2>;
+  return (
+    <form className={styles.loginForm} action="">
+      <h2>Login</h2>
+      <div className={styles.loginInputContainer}>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <div className={styles.loginInputContainer}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+      </div>
+      <div className={styles.loginInputContainer}>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <div className={styles.loginButtonContainer}>
+        <button>
+          <Link to="/">Back</Link>
+        </button>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
