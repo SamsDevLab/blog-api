87999fe7f7564f20e112882e6be5d1637be8d4fe
Sam Hadfield <sthtech@protonmail.com>
2026-08-31 13:34:21 -0500

    feat: get individual post for user to view/comment
    Query the post with the matching postId from the database. This allows
    users to view individual posts and comment on them. Add wireframe styling

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -1,7 +1,68 @@
-// import styles from "../Post/Post.module.css";
+import styles from "../Post/Post.module.css";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router";
 
 const Post = () => {
-  return <h2>You have reached a blog post!</h2>;
+  const { postId } = useParams();
+  const [selectedPost, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/posts/${postId}`);
+        if (response.ok === true) {
+          const postObject = await response.json();
+          setPost(postObject.targetedPost);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
+
+  // console.log(selectedPost);
+
+  return (
+    <div className={styles.postContainer}>
+      <div className={styles.postContent}>
+        {selectedPost === null ? (
+          <h2>Blog post cannot be found!</h2>
+        ) : (
+          <>
+            <h2>{selectedPost.title}</h2>
+            <h3>
+              {selectedPost.createdAt
+                ? `Created: ${new Date(selectedPost.createdAt).toLocaleString()}`
+                : "No date available"}
+            </h3>
+            <h3>
+              {selectedPost.updatedAt
+                ? `Updated: ${new Date(selectedPost.updatedAt).toLocaleString()}`
+                : "No date available"}
+            </h3>
+            <p>{selectedPost.content}</p>
+          </>
+        )}
+      </div>
+      <div className={styles.commentContainer}>
+        <form action="" method="" className={styles.commentForm}>
+          <div className={styles.commentInput}>
+            <label htmlFor="comment">Comment on Post</label>
+            <input type="textarea" id="comment" name="comment" />
+          </div>
+          <div className={styles.commentButtonContainer}>
+            <button>
+              <Link to="/">Back</Link>
+            </button>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
