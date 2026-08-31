7e6e4530caa48eb7438cdfa38309b7bf5fa24d9c
Sam Hadfield <sthtech@protonmail.com>
2026-08-31 16:22:28 -0500

    feat: add handleSubmit helper to return jwt token
    Add handleSubmit to return a JWT token for user upon successful login
    authentication.
    
    Remove 'username' field from the login form as email and password is
    sufficient for logging in

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -2,16 +2,27 @@ import styles from "../Login/Login.module.css";
 import { Link } from "react-router";
 
 const Login = () => {
+  async function handleSubmit(formData) {
+    const userData = Object.fromEntries(formData);
+    const response = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const { token } = await response.json();
+
+    localStorage.setItem("token", token);
+  }
+
   return (
-    <form className={styles.loginForm} action="">
+    <form className={styles.loginForm} action={handleSubmit}>
       <h2>Login</h2>
       <div className={styles.loginInputContainer}>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
-      </div>
-      <div className={styles.loginInputContainer}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
       </div>
       <div className={styles.loginInputContainer}>
         <label htmlFor="password">Password</label>
