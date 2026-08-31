138806fe195ec2db0167130aaf45832292823e5d
Sam Hadfield <sthtech@protonmail.com>
2026-08-31 13:28:48 -0500

    feat: fetch published posts and display in markup
    Fetch only published posts with a useEffect, then map through all posts
    and display them on the Home dashboard. Include Link component for routing
    postId so that user can click into post and comment.
    
    Add minimal, wireframe styling for easier viewing while developing

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,10 +1,48 @@
 import styles from "../Home/Home.module.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
 
 const Home = () => {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/posts/published");
+        if (response.ok === true) {
+          const postObject = await response.json();
+          setPosts(postObject.allPublishedPosts);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>Bloggin'</h2>
-      <h3>Blog posts will live here!</h3>
+      {posts === null ? (
+        <h2>No blog posts at the moment!</h2>
+      ) : (
+        posts.map((post) => {
+          return (
+            <div key={post.id} className={styles.blogPostCard}>
+              <h2>
+                <Link to={`/posts/${post.id}`}>{post.title}</Link>
+              </h2>
+              <h3>
+                {post.createdAt
+                  ? new Date(post.createdAt).toLocaleString()
+                  : "No date available"}
+              </h3>
+              <p>{post.content}</p>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
