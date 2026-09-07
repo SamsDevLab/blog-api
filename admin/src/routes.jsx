2efe614fdf76fc2cfdc94408a55c66c297e05f49
Sam Hadfield <sthtech@protonmail.com>
2026-09-07 14:46:09 -0500

    feat: import Home/Login - add as children

--- admin/src/routes.jsx
+++ admin/src/routes.jsx
@@ -1,9 +1,15 @@
 import App from "./App";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ];
 
