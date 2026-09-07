0681d505054a52189350889da45a8e5180f56f9f
Sam Hadfield <sthtech@protonmail.com>
2026-09-07 15:40:51 -0500

    feat: add form and wireframe styling
    Add login form and wireframe styling to the Login component

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -1,8 +1,25 @@
+import styles from "../Login/Login.module.css";
+import { Link } from "react-router";
+
 const Login = () => {
   return (
-    <>
-      <h2>You have reached the login page!</h2>
-    </>
+    <form action="" className={styles.loginForm}>
+      <h2>Admin Login</h2>
+      <div className={styles.inputContainer}>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </div>
+      <div className={styles.inputContainer}>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      <div className={styles.buttonContainer}>
+        <button>
+          <Link to="/">Back</Link>
+        </button>
+        <button>Submit</button>
+      </div>
+    </form>
   );
 };
 
