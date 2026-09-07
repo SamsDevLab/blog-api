f103e4f8209fee075ade93f2b16197be99f9e7b6
Sam Hadfield <sthtech@protonmail.com>
2026-09-07 14:44:08 -0500

    feat: add new components/stylesheets and export

---
+++ admin/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+import styles from "../Header/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <h1>
+        <Link to="/">Admin Dashboard</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ admin/src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h2>You have reached the login page!</h2>
+    </>
+  );
+};
+
+export default Login;
