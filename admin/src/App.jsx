6bc5a590bc108e4b69969937600a30080b2054d0
Sam Hadfield <sthtech@protonmail.com>
2026-09-07 14:47:08 -0500

    feat: import Header - add to appContainer

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
-
 import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h2>Admin Page</h2>
-    </>
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        <div className="main-container">
+          <Outlet />
+        </div>
+      </main>
+    </div>
   );
 }
 
