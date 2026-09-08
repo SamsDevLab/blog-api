68c2624e72950662e38012239757d94c59dadaff
Sam Hadfield <sthtech@protonmail.com>
2026-09-08 14:16:13 -0500

    feat: render posts, add handlePublished, style
    Render blog posts in JSX markup and add a button to each post to for
    toggling the 'published' status off and on.
    
    This will determine which blog posts are available to users in the client
    frontend and which posts are hidden from view.
    
    Do a little wireframe styling on blogPostCard divs

--- admin/src/components/Home/Home.jsx
+++ admin/src/components/Home/Home.jsx
@@ -1,5 +1,85 @@
+import styles from "../Home/Home.module.css";
+import { useState, useEffect } from "react";
+
 const Home = () => {
-  return <h2>You're Home!</h2>;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/posts");
+      const result = await response.json();
+      setPosts(result.allPosts);
+    }
+
+    fetchData();
+  }, []);
+
+  async function handlePublishedStatus(postId, publishedStatus) {
+    const reversePublishedStatus = !publishedStatus;
+
+    try {
+      const response = await fetch(`http://localhost:3000/posts/${postId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ published: reversePublishedStatus }),
+      });
+      if (response.ok === true) {
+        const result = await response.json();
+        const { updatedPost } = result;
+
+        const newArr = posts.map((post) => {
+          return post.id === updatedPost.id ? updatedPost : post;
+        });
+
+        setPosts(newArr);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      {posts === null ? (
+        <h2>No blog posts!</h2>
+      ) : (
+        posts.map((post) => {
+          return (
+            <div key={post.id} className={styles.blogPostCard}>
+              <h2>{post.title}</h2>
+              <h3>{new Date(post.createdAt).toLocaleString()}</h3>
+              <p>{post.content}</p>
+              {post.published === true ? (
+                <>
+                  <h3>Published</h3>
+                  <form
+                    action={() =>
+                      handlePublishedStatus(post.id, post.published)
+                    }
+                  >
+                    <button>Unpublish</button>
+                  </form>
+                </>
+              ) : (
+                <>
+                  <h3>Unpublished</h3>
+                  <form
+                    action={() =>
+                      handlePublishedStatus(post.id, post.published)
+                    }
+                  >
+                    <button>Publish</button>
+                  </form>
+                </>
+              )}
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 };
 
 export default Home;
