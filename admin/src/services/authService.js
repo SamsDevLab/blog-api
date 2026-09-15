f5994bbe4d6b79bef00b57f12ddf9e05d1f3a2cd
Sam Hadfield <sthtech@protonmail.com>
2026-09-15 09:49:01 -0500

    refactor: add 'admin' back to the fetch path

--- admin/src/services/authService.js
+++ admin/src/services/authService.js
@@ -1,5 +1,5 @@
 async function submitLogin(userData) {
-  const response = await fetch("http://localhost:3000/auth/login", {
+  const response = await fetch("http://localhost:3000/auth/admin/login", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
