80f2784f1ee4e828372368fc70db6271afe21139
Sam Hadfield <sthtech@protonmail.com>
2026-09-15 14:13:13 -0500

    feat: add Post component with stylesheet
    Add Post component so admins can click into individual posts

---
+++ admin/src/components/Post/Post.jsx
@@ -0,0 +1,67 @@
+import styles from "../Post/Post.module.css";
+import { useState, useEffect } from "react";
+import { Link, useParams, useOutletContext, useOutlet } from "react-router";
+import { fetchPostById } from "../../services/authService";
+
+const Post = () => {
+  const { postId } = useParams();
+  const [selectedPost, setPost] = useState(null);
+  const { token } = useOutletContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchPostById(postId, token);
+        if (response.ok === true) {
+          const postObject = await response.json();
+          setPost(postObject.targetedPost);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (token !== null) fetchData();
+  }, [postId, token]);
+
+  if (token === null) {
+    return (
+      <h2>
+        <Link to="/login">Login to view!</Link>
+      </h2>
+    );
+  } else {
+    return (
+      <div className={styles.postContainer}>
+        <div className={styles.postContent}>
+          {selectedPost === null ? (
+            <h2>Blog post cannot be found!</h2>
+          ) : (
+            <>
+              <h2>{selectedPost.title}</h2>
+              <h3>
+                {selectedPost.createdAt
+                  ? `Created: ${new Date(selectedPost.createdAt).toLocaleString()}`
+                  : "No date available"}
+              </h3>
+              <h3>
+                {selectedPost.updatedAt
+                  ? `Updated: ${new Date(selectedPost.updatedAt).toLocaleString()}`
+                  : "No date available"}
+              </h3>
+              <p>{selectedPost.content}</p>
+              {/* <Comments
+                loggedInUserId={selectedPost.loggedInUserId}
+                comments={selectedPost.comments}
+                onCommentSubmission={handleCommentSubmission}
+                onCommentDeletion={handleCommentDeletion}
+              /> */}
+            </>
+          )}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Post;
