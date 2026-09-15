0c632596d9fce673c24a97f25694c684e0e65cad
Sam Hadfield <sthtech@protonmail.com>
2026-09-15 15:39:42 -0500

    refactor: add message for post with no comments
    If the post has no comments, add a "No comments yet" header

--- admin/src/components/Comments/Comments.jsx
+++ admin/src/components/Comments/Comments.jsx
@@ -1,22 +1,26 @@
 import styles from "../Comments/Comments.module.css";
 
 const Comments = ({ comments, onCommentDeletion }) => {
-  return (
-    <div className={styles.commentContainer}>
-      {comments.map((comment) => {
-        return (
-          <div key={comment.id} className={styles.comment}>
-            <h3>{comment.author.username}</h3>
-            <h4>{`${new Date(comment.createdAt).toLocaleString()}`}</h4>
-            <p>{comment.content}</p>
-            <form action={() => onCommentDeletion(comment)}>
-              <button>Delete</button>
-            </form>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (comments.length === 0) {
+    return <h3>No comments yet</h3>;
+  } else {
+    return (
+      <div className={styles.commentContainer}>
+        {comments.map((comment) => {
+          return (
+            <div key={comment.id} className={styles.comment}>
+              <h3>{comment.author.username}</h3>
+              <h4>{`${new Date(comment.createdAt).toLocaleString()}`}</h4>
+              <p>{comment.content}</p>
+              <form action={() => onCommentDeletion(comment)}>
+                <button>Delete</button>
+              </form>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 export default Comments;
--- client/src/Components/Comments/Comments.jsx
+++ client/src/Components/Comments/Comments.jsx
@@ -7,24 +7,10 @@ const Comments = ({
   onCommentSubmission,
   onCommentDeletion,
 }) => {
-  return (
-    <div className={styles.commentContainer}>
-      {comments.map((comment) => {
-        return (
-          <div key={comment.id} className={styles.comment}>
-            <h3>{comment.author.username}</h3>
-            <h4>{`${new Date(comment.createdAt).toLocaleString()}`}</h4>
-            <p>{comment.content}</p>
-            {loggedInUserId === comment.authorId && (
-              <form action={() => onCommentDeletion(comment)}>
-                <button>Delete</button>
-              </form>
-            )}
-          </div>
-        );
-      })}
-
+  if (comments.length === 0) {
+    return (
       <form action={onCommentSubmission} className={styles.commentForm}>
+        <h3>No comments yet</h3>
         <div className={styles.commentInput}>
           <label htmlFor="comment">Comment on Post</label>
           <input type="textarea" id="comment" name="content" />
@@ -36,8 +22,40 @@ const Comments = ({
           <button>Submit</button>
         </div>
       </form>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.commentContainer}>
+        {comments.map((comment) => {
+          return (
+            <div key={comment.id} className={styles.comment}>
+              <h3>{comment.author.username}</h3>
+              <h4>{`${new Date(comment.createdAt).toLocaleString()}`}</h4>
+              <p>{comment.content}</p>
+              {loggedInUserId === comment.authorId && (
+                <form action={() => onCommentDeletion(comment)}>
+                  <button>Delete</button>
+                </form>
+              )}
+            </div>
+          );
+        })}
+
+        <form action={onCommentSubmission} className={styles.commentForm}>
+          <div className={styles.commentInput}>
+            <label htmlFor="comment">Comment on Post</label>
+            <input type="textarea" id="comment" name="content" />
+          </div>
+          <div className={styles.commentButtonContainer}>
+            <button>
+              <Link to="/">Back</Link>
+            </button>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Comments;
